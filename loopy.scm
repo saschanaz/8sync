d8527313ec9a47476020db8e82451746483492ee
Christopher Allan Webber <cwebber@dustycloud.org>
2015-11-17 21:50:34 -0600

    port mapping stuff, and don't fail on no arguments

--- loopy.scm
+++ loopy.scm
@@ -7,18 +7,45 @@
 ;;   replace this queue stuff with using pfds based immutable queues?
 
 (define-immutable-record-type <agenda>
-  (make-agenda-intern queue prompt-tag)
+  (make-agenda-intern queue prompt-tag port-mapping)
   agenda?
   (queue agenda-queue)
-  (prompt-tag agenda-prompt-tag))
+  (prompt-tag agenda-prompt-tag)
+  (port-mapping agenda-port-mapping))
 
 (define (make-async-prompt-tag)
   (make-prompt-tag "prompt"))
 
 (define* (make-agenda #:key
                       (queue (make-q))
-                      (prompt (make-prompt-tag)))
-  (make-agenda-intern queue prompt))
+                      (prompt (make-prompt-tag))
+                      (port-mapping (make-port-mapping)))
+  (make-agenda-intern queue prompt port-mapping))
+
+(define (make-port-mapping)
+  (make-hash-table))
+
+(define (port-mapping-set! port-mapping port #:optional read write except)
+  "Sets port-mapping for reader / writer / exception handlers"
+  (if (not (or read write except))
+      (throw 'no-handlers-given "No handlers given for port" port))
+  (hashq-set! port-mapping port
+              `#(,read ,write ,except)))
+
+(define (port-mapping-remove! port-mapping port)
+  (hashq-remove! port-mapping port))
+
+;; TODO: This is O(n), I'm pretty sure :\
+;; ... it might be worthwhile for us to have a
+;;   port-mapping record that keeps a count of how many
+;;   handlers (maybe via a promise?)
+(define (port-mapping-empty? port-mapping)
+  "Is this port mapping empty?"
+  (eq? (hash-count (const #t) port-mapping) 0))
+
+(define (port-mapping-non-empty? port-mapping)
+  "Whether this port-mapping contains any elements"
+  (not (port-mapping-empty? port-mapping)))
 
 (define %current-agenda (make-parameter #f))
 
@@ -61,7 +88,9 @@ based on the results"
            (for-each
             (lambda (new-proc)
               (enqueue new-proc))
-            new-procs)))))
+            new-procs))
+          ;; do nothing
+          (_ #f))))
     ;; TODO: Selecting on ports would happen here?
     ;; Return new agenda, with next queue set
     (set-field agenda (agenda-queue) next-queue)))
