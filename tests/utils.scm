5ad09886e8c3a1282c4f73e814085d545ef3ffae
Christopher Allan Webber <cwebber@dustycloud.org>
2015-11-28 09:28:12 -0600

    Licensed utils.scm under the LGPL, with permission from David Thompson

--- tests/utils.scm
+++ tests/utils.scm
@@ -1,20 +1,23 @@
-;;; Totes taken from dave
-
-;;; srt2vtt --- SRT to WebVTT converter
 ;;; Copyright © 2015 David Thompson <davet@gnu.org>
-;;;
-;;; srt2vtt is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or
-;;; (at your option) any later version.
-;;;
-;;; srt2vtt is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with srt2vtt.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This library is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public
+;; License as published by the Free Software Foundation; either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with this library; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301 USA
+
+;; Thanks for giving permission to license this under LGPL for
+;; consistency, David!
+
 
 (define-module (tests utils)
   #:use-module (srfi srfi-64)
