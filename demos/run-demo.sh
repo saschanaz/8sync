2c11c93e1c5033ed28ce5f2ba72167981bfa3224
Christopher Allan Webber <cwebber@dustycloud.org>
2015-11-29 11:50:31 -0600

    Add ./run-demo.sh script
    This script lets you run demos in this directory no matter where
    `guile' is installed on your system.
    
    * demos/run-demo.sh: New file

---
+++ demos/run-demo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# 8sync --- Asynchronous programming for Guile
+# Copyright (C) 2015 Christopher Allan Webber <cwebber@dustycloud.org>
+#
+# This file is part of 8sync.
+#
+# 8sync is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# 8sync is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with 8sync.  If not, see <http://www.gnu.org/licenses/>.
+
+# This script lets you run demos in this directory no matter where
+# `guile' is installed on your system.
+
+SCRIPTNAME=$1
+shift
+ARGS=$@
+
+env guile -l $SCRIPTNAME -e main -- $ARGS
